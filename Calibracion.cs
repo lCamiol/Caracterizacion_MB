4e44b2e0b46c666099e90a6003dcd11ab1bf7033
lCamiol <carlosorr@unicauca.edu.co>
2022-05-11 16:42:47 -0500

    CalibracionEsteban

--- Calibracion.cs
+++ Calibracion.cs
@@ -19,7 +19,7 @@ public class Calibracion : MonoBehaviour
     CultureInfo cultures = new CultureInfo("en-US");
     void Start()
     {
-        byte[] fileData = File.ReadAllBytes("Assets/Caracterizacion_MB/PruebaCalib.png");
+        byte[] fileData = File.ReadAllBytes("Assets/Caracterizacion_MB/Cam0.jpg");
         var tex = new Texture2D(2, 2);
         tex.LoadImage(fileData); // this will auto-resize the 2,2 texture dimensions.
         Mat map1 = OpenCvSharp.Unity.TextureToMat(tex);
@@ -42,14 +42,16 @@ public class Calibracion : MonoBehaviour
                     try{
                         decimal param= Convert.ToDecimal(arreglo[i], cultures);
                         DatosCamara.Add(param);
-                    }catch{
+                    Debug.Log(param);
+                    }
+                catch{
                         break;
                     }
                 }        
         }
 
 
-        /*Mat cameraMatrix =  new Mat(3, 3, MatType.CV_64F);
+       /* Mat cameraMatrix =  new Mat(3, 3, MatType.CV_64FC1);
         cameraMatrix.Set(0, 0, DatosCamara[4]);
         cameraMatrix.Set(0, 1, DatosCamara[5]);
         cameraMatrix.Set(0, 2, DatosCamara[6]);;
@@ -60,14 +62,14 @@ public class Calibracion : MonoBehaviour
         cameraMatrix.Set(2, 1, DatosCamara[11]);
         cameraMatrix.Set(2, 2, DatosCamara[12]);
 
-        Mat distCoeffs = new Mat(5, 1, MatType.CV_64F);
+        Mat distCoeffs = new Mat(5, 1, MatType.CV_64FC1);
         distCoeffs.Set(0, 0, DatosCamara[0]);
         distCoeffs.Set(1, 0, DatosCamara[1]);
         distCoeffs.Set(2, 0, DatosCamara[2]);
         distCoeffs.Set(3, 0, DatosCamara[3]);
-        distCoeffs.Set(4, 0, 0);*/
+        distCoeffs.Set(4, 0, 0);*/ 
 
-        Mat cameraMatrix = new Mat(3, 3, MatType.CV_64F);
+        /*Mat cameraMatrix = new Mat(3, 3, MatType.CV_64F);
         cameraMatrix.Set(0, 0, 467.420998429215);
         cameraMatrix.Set(0, 1, 0);
         cameraMatrix.Set(0, 2, 0);
@@ -76,14 +78,14 @@ public class Calibracion : MonoBehaviour
         cameraMatrix.Set(1, 2, 0);
         cameraMatrix.Set(2, 0, 320.721360122971);
         cameraMatrix.Set(2, 1, 180.458856691086);
-        cameraMatrix.Set(2, 2, 0);
+        cameraMatrix.Set(2, 2, 1);
 
         Mat distCoeffs = new Mat(5, 1, MatType.CV_64F);
         distCoeffs.Set(0, 0, 0.0229184164434659);
         distCoeffs.Set(1, 0, -0.122241761248509);
         distCoeffs.Set(2, 0, 0);
         distCoeffs.Set(3, 0, 0);
-        distCoeffs.Set(4, 0, 0);
+        distCoeffs.Set(4, 0, 0);*/
 
         //Mat.ForeachFunctionDouble(cameraMatrix);
 
@@ -105,18 +107,34 @@ public class Calibracion : MonoBehaviour
         distCoeffs[3, 0] = 0;
         distCoeffs[4, 0] = 0;*/
 
-
+        var size = new Size(9, 6);
+        var rms = 0.0;
+        var camera = new MatOfDouble(Mat.Eye(3, 3, MatType.CV_64FC1));
+        Cv2.CvtColor(map1, map1, ColorConversionCodes.BGR2GRAY);
+        var criteria = new TermCriteria(CriteriaType.Eps | CriteriaType.MaxIter, 30, 0.001);
+        var imgPoints = new List<MatOfPoint2f>();
+        var objPoints = new List<MatOfPoint3f>();
+        var distortion = new Mat();
+        var frameSize = Size.Zero;
         Mat map2 = map1.Clone();
         Mat newcam1 = new Mat();
+        //var objp = MatOfPoint3f.FromArray(Create3DChessboardCorners(size, 1));
+        Cv2.FindChessboardCorners(map1, size, out Point2f[] corners);
+        //objPoints.Add(objp);
+        UnityEngine.Debug.Log("Esquinas"+ corners.Length);
+        imgPoints.Add(MatOfPoint2f.FromArray(corners.ToArray()));
+        //var corners2 = Cv2.CornerSubPix(map1, corners, size, size, criteria);
+        //Cv2.DrawChessboardCorners(map1, map1.Size(), corners2, true);
 
-        //newcam1 = Cv2.GetOptimalNewCameraMatrix(cameraMatrix, distCoeffs, map1.Size(),1, map1.Size(), 0);
+        rms = Cv2.CalibrateCamera(objPoints, imgPoints, map1.Size(), camera, distortion, out var rvectors, out var tvectors, CalibrationFlags.UseIntrinsicGuess | CalibrationFlags.FixK5);
+        newcam1 = Cv2.GetOptimalNewCameraMatrix(camera, distortion, map1.Size(),1, map1.Size(),out var roi);
         //Cv2.InitUndistortRectifyMap(cameraMatrix, distCoeffs, new Mat(), newcam1, Size(700, 680), map1, map2);
-        Cv2.Undistort(map1,map2,cameraMatrix, distCoeffs);
+        Cv2.Undistort(map1,map2, camera, distortion, newcam1);
 
-        Cv2.ImShow("Imagen normal", map2);
-        //Cv2.ImShow("imagen calibrada", map2);
+        Cv2.ImShow("Imagen normal", map1);
+        Cv2.ImShow("imagen calibrada", map2);
         //UnityEngine.Debug.Log("Matriz de distorsion" + cameraMatrix.ToString());
-        UnityEngine.Debug.Log("coeficiente de dst"+ distCoeffs);
+        //UnityEngine.Debug.Log("coeficiente de dst");
     }
     // Update is called once per frame
     void Update()
